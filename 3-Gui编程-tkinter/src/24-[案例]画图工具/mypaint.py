7eee375ee7dd41c05b196e1bdb7da2e5356f0cbc
YG-07 <1030138554@qq.com>
2021-01-26 18:56:14 +0800

    'update'

--- 3-Gui编程-tkinter/src/24-[案例]画图工具/mypaint.py
+++ 3-Gui编程-tkinter/src/24-[案例]画图工具/mypaint.py
@@ -9,6 +9,7 @@
 # 6.修改画笔颜色、背景颜色
 
 from tkinter import *
+from tkinter.ttk import *
 from tkinter.colorchooser import askcolor
 from tkinter.filedialog import *
 from tkinter.messagebox import *
@@ -22,7 +23,14 @@ class Application(Frame):
         super().__init__(master)    #super()是父类的构造器
         self.master = master
         self.pack()
+        self.createData()
         self.createWidget()
+
+    def createData(self):
+        self.sx = 0
+        self.sy = 0
+        self.lastLine = 0
+        self.startLine = False
 
     def createWidget(self):
         # 创建组件
@@ -32,12 +40,76 @@ class Application(Frame):
         self.c.create_oval(600, 500, 150, 160, fill='green')
         self.c.create_oval(900, 900, 150, 160, fill='red')
 
+        # 创建画图工具
+        self.erasor = Button(self, text='橡皮擦', name='erasor', width=8)
+        self.erasor.pack(side='left')
+        self.pen = Button(self, text='画笔', name='pen', width=8)
+        self.pen.pack(side='left')
+        self.line = Button(self, text='直线', name='line', width=8)
+        self.line.pack(side='left')
+        self.arrowLine = Button(self, text='箭头直线', name='arrowLine', width=8)
+        self.arrowLine.pack(side='left')
+        self.circle = Button(self, text='圆', name='circle', width=8)
+        self.circle.pack(side='left')
+        self.rect = Button(self, text='矩形', name='rect', width=8)
+        self.rect.pack(side='left')
+        self.color = Button(self, text='画笔颜色', name='color', width=8)
+        self.color.pack(side='left')
+        self.v1 = '#000'
+        self.v2 = '#fff'
+        self.fgc = Label(self, width=2, height=1, bg=self.v1, borderwidth=1, relief='solid')
+        self.fgc.pack(side='left', padx=3)
+        self.bgc = Label(self, width=2, height=1, bg=self.v2, borderwidth=1, relief='solid')
+        self.bgc.pack(side='left')
+        Separator(self, orient='vertical').pack(side='left', fill='y', padx=20)
 
         # 创建功能按钮
+        self.clear = Button(self, text='清屏', name='clear')
+        self.clear.pack(side='left')
         self.save = Button(self, text='保存', name='save', command=self.getter)
-        self.save.pack(side='left', padx=10)
+        self.save.pack(side='left')
         self.open = Button(self, text='打开', name='open', command=self.setter)
-        self.open.pack(side='left', padx=10)
+        self.open.pack(side='left')
+
+        # 鼠标事件处理
+        self.master.bind_class('Button','<1>', self.eventMag)
+        self.c.bind('<ButtonRelease-1>', self.stopDraw)
+
+
+    # 画图功能函数,事件管理
+    def eventMag(self, event):
+        name = event.widget.winfo_name()
+        print(name)
+        if name=='line':
+            self.c.bind('<B1-Motion>', self.myLine)
+        elif name=='arrowLine':
+            self.c.bind('<B1-Motion>', self.myArrLine)
+        elif name=='rect':
+            self.c.bind('<B1-Motion>', self.myRect)
+
+    def startDraw(self, event):
+        self.c.delete(self.lastLine)
+        if not self.startLine:
+            self.startLine = True
+            self.sx = event.x
+            self.sy = event.y
+    # 画图开始函数
+    def stopDraw(self, event):
+        self.startLine = False
+        self.lastLine = 0
+    # 直线
+    def myLine(self, event):
+        self.startDraw(event)
+        self.lastLine=self.c.create_line(self.sx, self.sy, event.x, event.y, fill=self.v1)
+    # 箭头直线
+    def myArrLine(self, event):
+        self.startDraw(event)
+        self.lastLine=self.c.create_line(self.sx, self.sy, event.x, event.y, arrow=LAST, fill=self.v1)
+    # 矩形
+    def myRect(self, event):
+        self
+
+    # 功能函数
 
     def getter(self):
         self.c.update()
--- README.md
+++ README.md
@@ -5,3 +5,55 @@ a python desktop video program by the tkinter package
 1. 桌面视频URL解析程序 B站视频：https://www.bilibili.com/video/BV1GA411H7Nr
 2. tkinter部件详解 B站视频：https://www.bilibili.com/video/BV1nZ4y1M7F6
 3. python的GUI编程之tkinter B站视频：https://www.bilibili.com/video/BV1zE411g7LY?p=1
+
+# Tk案例：画图程序思路
+## 一、程序基本结构
+### 1.1 使用Tk程序的class封装代码
+### 1.2 创建组件
+1. 创建画布Canvas，并pack布局，fill横纵轴
+```python
+self.c = Canvas(self.master, width=450, height=280, bg='#fff')
+self.c.pack(fill='both',expand=1)
+```
+2. 创建各种画图和功能按钮
+## 二、处理事件
+### 2.1 鼠标事件处理
+```python
+self.master.bind_class('Button','<1>', self.eventMag)
+self.c.bind('<ButtonRelease-1>', self.stopDraw)
+```
+### 2.2 实现事件管理函数eventMag
+```python
+# 画图功能函数,事件管理
+def eventMag(self, event):
+    name = event.widget.winfo_name()
+    print(name)
+    # 1.画直线
+    if name=='line':
+        self.c.bind('<B1-Motion>', self.myLine)
+```
+#### 2.2.1 画直线思路
+1. 监听鼠标按住事件`myline()`
+2. 先假设从(0,0)开始画线，①先解决重复画线的问题，通过设置一个`lastLine`返回每次画的线，每次调用按住事件时删除一个上一次的线.
+3. ②再解决开始画线坐标问题，再设置一个`startLine`的布尔值,在第一次调用按住事件获取点击事件的坐标(x,y),再改变startLine的值.
+4. ③解决释放按键后，可以重新开始画线，设置一个`stopDraw`绑定释放鼠标事件，将`startLine和lastLine设置为初始值`
+```python
+def myLine(self, event):
+    self.c.delete(self.lastLine)
+    if not self.startLine:
+        self.startLine = True
+        self.sx = event.x
+        self.sy = event.y
+    self.lastLine=self.c.create_line(self.sx, self.sy, event.x, event.y, fill=self.v1)
+def stopDraw(self, event):
+    self.startLine = False
+    self.lastLine = 0
+```
+#### 2.2.2 箭头直线思路
+1. 跟画直线结果一模一样，只用在`create_line`函数里添加`arrow=LAST`参数，表示在直线结束添加箭头
+2. 将画直线的共同的代码封装成startDraw函数
+```python
+def myArrLine(self, event):
+    self.startDraw(event)
+    self.lastLine=self.c.create_line(self.sx, self.sy, event.x, event.y, arrow=LAST, fill=self.v1)
+```
